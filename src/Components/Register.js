ed15fc4095a93181d3560a1bc69e9a56d5fb17cd
Venkat5452 <venkatsai.bandi2019@gmail.com>
2023-12-19 16:06:20 +0530

    Update Register.js

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -142,7 +142,7 @@ function Register() {
   return (
     <div className='container d-flex align-items-center justify-content-center w-50 rounded-4 backss'>
         <div className="p-2 w-75 mx-auto">
-            <h3 className="w-30"><h3 className="text-success">🧘‍♂️ YOGA 🧘‍♀️</h3>Registration Form</h3>
+            <h3 className="w-30"><h3 className="text-success">🧘‍♂️ YOGA Class 🧘‍♀️</h3>Registration Form</h3>
             <Image src={img} alt="im" className="w-75 h-50 p-1 rounded-4"></Image>
             {err!=="" && !flag1 && <h5 className="text-danger">{err}</h5>}
             {flagl && <h6 className="text-success">Loading..</h6>}
